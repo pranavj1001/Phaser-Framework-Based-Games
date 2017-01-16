d123b63445636c51d142e63a4a98602a455e559e
pranavj1001 <pranavj1001@gmail.com>
2017-01-16 20:01:36 +0530

    initialize the main.js

---
+++ Animals/js/main.js
@@ -0,0 +1,25 @@
+ //create a new Game instance
+ //set the game dimensions and set the Phaser.AUTO (OpenGL or Canvas)
+ var game = new Phaser.Game(640, 360, Phaser.AUTO);
+
+//create a game state (this contains the logic of the game)
+var GameState = {
+	//assets are loaded in this function
+	preload: function(){
+
+	},
+	//after preloading create function is called
+	create: function(){
+
+	},
+	//this function is called multiple times to handle the requests when game is live
+	update: function(){
+
+	}
+};
+
+//add state to the main game
+game.state.add('GameState', GameState);
+
+//to launch the game
+game.state.start('GameState');
